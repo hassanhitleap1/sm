d0af0c8a8eb62e3806851624058ef559f97d602f
hassanhitleap1 <34882574+hassanhitleap1@users.noreply.github.com>
2021-01-28 14:41:31 +0200

    Update restaurant.blade.php
    qrlanding.restaurant-information

--- resources/views/cart/restaurant.blade.php
+++ resources/views/cart/restaurant.blade.php
@@ -1,7 +1,7 @@
 <div class="card card-profile shadow">
     <div class="px-4">
       <div class="mt-5">
-        <h3>{{ __('Restaurant information') }}<span class="font-weight-light"></span></h3>
+        <h3>{{ __('qrlanding.restaurant-information') }}<span class="font-weight-light"></span></h3>
       </div>
       <div class="card-content border-top">
         <br />
