a92f2a66b5e1a87a4219e143fe55264a4bae4362
hassanhitleap1 <34882574+hassanhitleap1@users.noreply.github.com>
2021-01-28 14:43:21 +0200

    Update newaddress.blade.php
    qrlanding

--- resources/views/cart/newaddress.blade.php
+++ resources/views/cart/newaddress.blade.php
@@ -1,7 +1,7 @@
 <div class="card card-profile shadow" id="addressBox">
     <div class="px-4">
       <div class="mt-5">
-        <h3>{{ __('Delivery Address') }}<span class="font-weight-light"></span></h3>
+        <h3>{{ __('qrlanding.delivery-address') }}<span class="font-weight-light"></span></h3>
       </div>
       <div class="card-content border-top">
         <br />
