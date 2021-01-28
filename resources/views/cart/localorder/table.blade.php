afa0a6b2d839ee36b8c6579605bf9daa860d7281
hassanhitleap1 <34882574+hassanhitleap1@users.noreply.github.com>
2021-01-28 15:08:19 +0200

    Update table.blade.php
    qrlanding

--- resources/views/cart/localorder/table.blade.php
+++ resources/views/cart/localorder/table.blade.php
@@ -1,7 +1,7 @@
 <div class="card card-profile shadow tablepicker">
     <div class="px-4">
       <div class="mt-5">
-        <h3>{{ __('Table') }}<span class="font-weight-light"></span></h3>
+        <h3>{{ __('qrlanding.table') }}<span class="font-weight-light"></span></h3>
       </div>
       <div class="card-content border-top">
         <br />
