86398220aba7bfdc4ac6c7209a45f4331b8a0071
hassanhitleap1 <34882574+hassanhitleap1@users.noreply.github.com>
2021-01-28 14:40:38 +0200

    Update comment.blade.php
    comment

--- resources/views/cart/comment.blade.php
+++ resources/views/cart/comment.blade.php
@@ -2,12 +2,12 @@
 <div class="card card-profile shadow">
     <div class="px-4">
       <div class="mt-5">
-        <h3>{{ __('Comment') }}<span class="font-weight-light"></span></h3>
+        <h3>{{ __('qrlanding.comment') }}<span class="font-weight-light"></span></h3>
       </div>
       <div class="card-content border-top">
         <br />
         <div class="form-group{{ $errors->has('comment') ? ' has-danger' : '' }}">
-            <textarea name="comment" id="comment" class="form-control{{ $errors->has('comment') ? ' is-invalid' : '' }}" placeholder="{{ __( 'Your comment here' ) }} ..."></textarea>
+            <textarea name="comment" id="comment" class="form-control{{ $errors->has('comment') ? ' is-invalid' : '' }}" placeholder="{{ __( 'qrlanding.your-comment-here' ) }} ..."></textarea>
             @if ($errors->has('comment'))
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $errors->first('comment') }}</strong>
