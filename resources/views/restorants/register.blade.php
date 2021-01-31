df845855fac0aedb839b87376cb59d2e7ee67c6c
hassanhitleap1 <34882574+hassanhitleap1@users.noreply.github.com>
2021-01-31 13:59:25 +0200

    Update register.blade.php
    qrlanding

--- resources/views/restorants/register.blade.php
+++ resources/views/restorants/register.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.front', ['title' => __('User Profile')])
+@extends('layouts.front', ['title' => __('qrlanding.user-profile')])
 @if (strlen(env('RECAPTCHA_SITE_KEY',""))>2)
     @section('head')
     {!! htmlScriptTagJsApi([]) !!}
@@ -19,14 +19,14 @@
                 <div class="card bg-secondary shadow">
                     <div class="card-header bg-white border-0">
                         <div class="row align-items-center">
-                            <h3 class="col-12 mb-0">{{ __('Register your restaurant') }}</h3>
+                            <h3 class="col-12 mb-0">{{ __('qrlanding.register-your-restaurant') }}</h3>
                         </div>
                     </div>
                     <div class="card-body">
                         <form  id="registerform" method="post" action="{{ route('newrestaurant.store') }}" autocomplete="off">
                             @csrf
 
-                            <h6 class="heading-small text-muted mb-4">{{ __('Restaurant information') }}</h6>
+                            <h6 class="heading-small text-muted mb-4">{{ __('qrlanding.restaurant-information') }}</h6>
 
                             @if (session('status'))
                                 <div class="alert alert-success alert-dismissible fade show" role="alert">
@@ -38,7 +38,7 @@
                             @endif
                             <div class="pl-lg-4">
                                 <div class="form-group{{ $errors->has('name') ? ' has-danger' : '' }}">
-                                    <label class="form-control-label" for="name">{{ __('Restaurant Name') }}</label>
+                                    <label class="form-control-label" for="name">{{ __('qrlanding.restaurant-name') }}</label>
                                     <input type="text" name="name" id="name" class="form-control form-control-alternative{{ $errors->has('name') ? ' is-invalid' : '' }}" placeholder="{{ __('Restaurant Name here') }} ..." value="{{ isset($_GET["name"])?$_GET['name']:""}}" required autofocus>
                                     @if ($errors->has('name'))
                                         <span class="invalid-feedback" role="alert">
@@ -48,11 +48,11 @@
                                 </div>
                             </div>
                             <hr class="my-4" />
-                            <h6 class="heading-small text-muted mb-4">{{ __('Owner information') }}</h6>
+                            <h6 class="heading-small text-muted mb-4">{{ __('qrlanding.owner-information') }}</h6>
                             <div class="pl-lg-4">
                                 <div class="form-group{{ $errors->has('name_owner') ? ' has-danger' : '' }}">
-                                    <label class="form-control-label" for="name_owner">{{ __('Owner Name') }}</label>
-                                    <input type="text" name="name_owner" id="name_owner" class="form-control form-control-alternative{{ $errors->has('name_owner') ? ' is-invalid' : '' }}" placeholder="{{ __('Owner Name here') }} ..." value="{{ isset($_GET["name"])?$_GET['name']:""}}" required autofocus>
+                                    <label class="form-control-label" for="name_owner">{{ __('qrlanding.owner-name') }}</label>
+                                    <input type="text" name="name_owner" id="name_owner" class="form-control form-control-alternative{{ $errors->has('name_owner') ? ' is-invalid' : '' }}" placeholder="{{ __('qrlanding.owner-name') }} ..." value="{{ isset($_GET["name"])?$_GET['name']:""}}" required autofocus>
 
                                     @if ($errors->has('name_owner'))
                                         <span class="invalid-feedback" role="alert">
@@ -61,8 +61,8 @@
                                     @endif
                                 </div>
                                 <div class="form-group{{ $errors->has('email_owner') ? ' has-danger' : '' }}">
-                                    <label class="form-control-label" for="email_owner">{{ __('Owner Email') }}</label>
-                                    <input type="email" name="email_owner" id="email_owner" class="form-control form-control-alternative{{ $errors->has('email_owner') ? ' is-invalid' : '' }}" placeholder="{{ __('Owner Email here') }} ..." value="{{ isset($_GET["email"])?$_GET['email']:""}}" required autofocus>
+                                    <label class="form-control-label" for="email_owner">{{ __('qrlanding.owner-email') }}</label>
+                                    <input type="email" name="email_owner" id="email_owner" class="form-control form-control-alternative{{ $errors->has('email_owner') ? ' is-invalid' : '' }}" placeholder="{{ __('qrlanding.owner-email') }} ..." value="{{ isset($_GET["email"])?$_GET['email']:""}}" required autofocus>
 
                                     @if ($errors->has('email_owner'))
                                         <span class="invalid-feedback" role="alert">
@@ -71,8 +71,8 @@
                                     @endif
                                 </div>
                                 <div class="form-group{{ $errors->has('phone_owner') ? ' has-danger' : '' }}">
-                                    <label class="form-control-label" for="phone_owner">{{ __('Owner Phone') }}</label>
-                                    <input type="text" name="phone_owner" id="phone_owner" class="form-control form-control-alternative{{ $errors->has('phone_owner') ? ' is-invalid' : '' }}" placeholder="{{ __('Owner Phone here') }} ..." value="{{ isset($_GET["phone"])?$_GET['phone']:""}}" required autofocus>
+                                    <label class="form-control-label" for="phone_owner">{{ __('qrlanding.owner-phone') }}</label>
+                                    <input type="text" name="phone_owner" id="phone_owner" class="form-control form-control-alternative{{ $errors->has('phone_owner') ? ' is-invalid' : '' }}" placeholder="{{ __('qrlanding.owner-phone') }} ..." value="{{ isset($_GET["phone"])?$_GET['phone']:""}}" required autofocus>
 
                                     @if ($errors->has('phone_owner'))
                                         <span class="invalid-feedback" role="alert">
@@ -89,9 +89,9 @@
                                         </span>
                                         @endif
 
-                                        {!! htmlFormButton(__('Save'), ['id'=>'thesubmitbtn','class' => 'btn btn-success mt-4']) !!}
+                                        {!! htmlFormButton(__('qrlanding.save'), ['id'=>'thesubmitbtn','class' => 'btn btn-success mt-4']) !!}
                                     @else
-                                        <button type="submit" id="thesubmitbtn" class="btn btn-success mt-4">{{__('Save')}}</button>
+                                        <button type="submit" id="thesubmitbtn" class="btn btn-success mt-4">{{__('qrlanding.save')}}</button>
                                     @endif
 
 
