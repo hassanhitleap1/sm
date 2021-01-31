4ab38c37e1f62c901c0c2e782cfcbbe519d98d34
hassanhitleap1 <34882574+hassanhitleap1@users.noreply.github.com>
2021-01-31 16:23:27 +0200

    Update index.blade.php
    qrlanding.

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app', ['title' => __('User Management')])
+@extends('layouts.app', ['title' => __('qrlanding.user-management')])
 
 @section('content')
     <div class="header bg-gradient-primary pb-8 pt-5 pt-md-8">
@@ -11,10 +11,10 @@
                     <div class="card-header border-0">
                         <div class="row align-items-center">
                             <div class="col-8">
-                                <h3 class="mb-0">{{ __('Users') }}</h3>
+                                <h3 class="mb-0">{{ __('qrlanding.users') }}</h3>
                             </div>
                             <div class="col-4 text-right">
-                                <a href="{{ route('user.create') }}" class="btn btn-sm btn-primary">{{ __('Add user') }}</a>
+                                <a href="{{ route('user.create') }}" class="btn btn-sm btn-primary">{{ __('qrlanding.add-user') }}</a>
                             </div>
                         </div>
                     </div>
@@ -27,9 +27,9 @@
                         <table class="table align-items-center table-flush">
                             <thead class="thead-light">
                                 <tr>
-                                    <th scope="col">{{ __('Name') }}</th>
-                                    <th scope="col">{{ __('Email') }}</th>
-                                    <th scope="col">{{ __('Creation Date') }}</th>
+                                    <th scope="col">{{ __('qrlanding.name') }}</th>
+                                    <th scope="col">{{ __('qrlanding.email') }}</th>
+                                    <th scope="col">{{ __('qrlanding.creation-date') }}</th>
                                     <th scope="col"></th>
                                 </tr>
                             </thead>
@@ -52,13 +52,13 @@
                                                             @csrf
                                                             @method('delete')
                                                             
-                                                            <a class="dropdown-item" href="{{ route('user.edit', $user) }}">{{ __('Edit') }}</a>
+                                                            <a class="dropdown-item" href="{{ route('user.edit', $user) }}">{{ __('qrlanding.edit') }}</a>
                                                             <button type="button" class="dropdown-item" onclick="confirm('{{ __("Are you sure you want to delete this user?") }}') ? this.parentElement.submit() : ''">
-                                                                {{ __('Delete') }}
+                                                                {{ __('qrlanding.delete') }}
                                                             </button>
                                                         </form>    
                                                     @else
-                                                        <a class="dropdown-item" href="{{ route('profile.edit') }}">{{ __('Edit') }}</a>
+                                                        <a class="dropdown-item" href="{{ route('profile.edit') }}">{{ __('qrlanding.edit') }}</a>
                                                     @endif
                                                 </div>
                                             </div>
