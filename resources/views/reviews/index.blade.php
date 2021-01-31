57ae05014cac4e4f1f96a4a4014f52802a8d2dec
hassanhitleap1 <34882574+hassanhitleap1@users.noreply.github.com>
2021-01-31 12:41:22 +0200

    Update index.blade.php
    qrlanding.

--- resources/views/reviews/index.blade.php
+++ resources/views/reviews/index.blade.php
@@ -1,10 +1,10 @@
 @extends('general.index', $setup);
 @section('thead')
-    <th>{{ __('Rating') }}</th>
-    <th>{{ __('Comment') }}</th>
-    <th>{{ __('Order') }}</th>
-    <th>{{ __('User') }}</th>
-    <th>{{ __('Actions') }}</th>
+    <th>{{ __('qrlanding.rating') }}</th>
+    <th>{{ __('qrlanding.comment') }}</th>
+    <th>{{ __('qrlanding.order') }}</th>
+    <th>{{ __('qrlanding.user') }}</th>
+    <th>{{ __('qrlanding.actions') }}</th>
 @endsection
 @section('tbody')
 @foreach ($setup['items'] as $item)
@@ -13,7 +13,7 @@
     <td>{{ $item->comment }}</td>
 <td><a href="{{ route('orders.show',['order'=>$item->order->id]) }}">{{ "#".$item->order->id }}</a></td>
     <td>{{ $item->user->name }}</td>
-    <td><a href="{{ route("reviews.destroyget",["rating"=>$item->id]) }}" class="btn btn-danger btn-sm">{{ __('Delete') }}</a></td>
+    <td><a href="{{ route("reviews.destroyget",["rating"=>$item->id]) }}" class="btn btn-danger btn-sm">{{ __('qrlanding.delete') }}</a></td>
 </tr> 
 @endforeach
 
