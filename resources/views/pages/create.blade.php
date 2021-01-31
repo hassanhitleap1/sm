bd749ea29ba3d13f013c0d8693a684a0c4507484
hassanhitleap1 <34882574+hassanhitleap1@users.noreply.github.com>
2021-01-31 09:59:09 +0200

    Update create.blade.php
    qrlanding

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app', ['title' => __('Page')])
+@extends('layouts.app', ['title' => __('qrlanding.page')])
 
 @section('content')
 <div class="header bg-gradient-primary pb-8 pt-5 pt-md-8">
@@ -10,15 +10,15 @@
                 <div class="card-header bg-white border-0">
                     <div class="row align-items-center">
                         <div class="col-8">
-                            <h3 class="mb-0">{{ __('Page Management') }}</h3>
+                            <h3 class="mb-0">{{ __('qrlanding.page-management') }}</h3>
                         </div>
                         <div class="col-4 text-right">
-                            <a href="{{ route('pages.index') }}" class="btn btn-sm btn-primary">{{ __('Back to pages') }}</a>
+                            <a href="{{ route('pages.index') }}" class="btn btn-sm btn-primary">{{ __('qrlanding.back-to-pages') }}</a>
                         </div>
                     </div>
                 </div>
                 <div class="card-body">
-                    <h6 class="heading-small text-muted mb-4">{{ __('Page information') }}</h6>
+                    <h6 class="heading-small text-muted mb-4">{{ __('qrlanding.page-information') }}</h6>
                     @if (session('status'))
                     <div class="alert alert-success alert-dismissible fade show" role="alert">
                         {{ session('status') }}
@@ -40,11 +40,11 @@
                                 @endif
                             </div>
                             <div class="form-group">
-                            <label class="form-control-label" for="ckeditor">{{ __('Content') }}</label>
+                            <label class="form-control-label" for="ckeditor">{{ __('qrlanding.content') }}</label>
                                 <textarea class="form-control" id="ckeditor" name="ckeditor"></textarea>
                             </div>
                             <div class="text-center">
-                                <button type="submit" class="btn btn-success mt-4">{{ __('Save') }}</button>
+                                <button type="submit" class="btn btn-success mt-4">{{ __('qrlanding.save') }}</button>
                             </div>
                         </form>
                     </div>
