4aa12f6ba0039a130921a5af3c7c672c272074d0
hassanhitleap1 <34882574+hassanhitleap1@users.noreply.github.com>
2021-01-31 13:56:21 +0200

    Update share.blade.php
    qrlanding

--- resources/views/restorants/share.blade.php
+++ resources/views/restorants/share.blade.php
@@ -1,6 +1,6 @@
-@extends('layouts.app', ['title' => __('Share Menu')])
+@extends('layouts.app', ['title' => __('qrlanding.share-menu')])
 @section('admin_title')
-    {{__('Share Menu')}}
+    {{__('qrlanding.share-menu')}}
 @endsection
 @section('title')
 <title>{{$name}}</title>
@@ -19,14 +19,14 @@
             <div class="col-xl-12 order-xl-1">
                 <div class="card bg-secondary shadow">
                     <div class="card-body">
-                        <h2 class="text-uppercase text-center text-muted mb-4">{{ __('Share your menu with your audience') }}</h2>
+                        <h2 class="text-uppercase text-center text-muted mb-4">{{ __('qrlanding.share-your-menu-with-your-audience') }}</h2>
                         <div class="pl-lg-4">
                             <div class="sharethis-inline-share-buttons" data-url="{{ $url }}"></div>
                             <div class="text-center mt-6">
                                 <img src="{{ 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data='.$url }}" class="image mr-3" alt=""/>
                             </div>
                             <div class="text-center mt-4 mr-3">
-                                <a type="button" href="/downloadqr" class="btn btn-success">{{ __('Download QR Code') }}</a>
+                                <a type="button" href="/downloadqr" class="btn btn-success">{{ __('qrlanding.download-QR-code') }}</a>
                             </div>
                         </div>
                     </div>
