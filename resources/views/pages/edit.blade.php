2c89a5127131d9c22afdfa8b3bfec77b0133da1b
hassanhitleap1 <34882574+hassanhitleap1@users.noreply.github.com>
2021-01-31 12:28:21 +0200

    Update edit.blade.php
    qrlanding

--- resources/views/pages/edit.blade.php
+++ resources/views/pages/edit.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app', ['title' => __('Page')])
+@extends('layouts.app', ['title' => __('qrlanding.page')])
 
 @section('content')
 <div class="header bg-gradient-primary pb-8 pt-5 pt-md-8">
@@ -10,15 +10,15 @@
                 <div class="card-header bg-white border-0">
                     <div class="row align-items-center">
                         <div class="col-8">
-                            <h3 class="mb-0">{{ __('Page Management') }}</h3>
+                            <h3 class="mb-0">{{ __('qrlanding.page-,anagement') }}</h3>
                         </div>
                         <div class="col-4 text-right">
-                            <a href="{{ route('pages.index') }}" class="btn btn-sm btn-primary">{{ __('Back to pages') }}</a>
+                            <a href="{{ route('pages.index') }}" class="btn btn-sm btn-primary">{{ __('qrlanding.back-to-pages') }}</a>
                         </div>
                     </div>
                 </div>
                 <div class="card-body">
-                    <h6 class="heading-small text-muted mb-4">{{ __('Page information') }}</h6>
+                    <h6 class="heading-small text-muted mb-4">{{ __('qrlanding.page-information') }}</h6>
                     @if (session('status'))
                     <div class="alert alert-success alert-dismissible fade show" role="alert">
                         {{ session('status') }}
@@ -32,7 +32,7 @@
                             @csrf
                             @method('put')
                             <div class="form-group{{ $errors->has('title') ? ' has-danger' : '' }}">
-                                <label class="form-control-label" for="title">{{ __('Title') }}</label>
+                                <label class="form-control-label" for="title">{{ __('qrlanding.title') }}</label>
                                 <input type="text" name="title" id="title" class="form-control form-control-alternative{{ $errors->has('title') ? ' is-invalid' : '' }}" placeholder="{{ __('Title here ...') }}" value="{{ old('title', $page->title) }}" required autofocus>
                                 @if ($errors->has('title'))
                                 <span class="invalid-feedback" role="alert">
@@ -41,11 +41,11 @@
                                 @endif
                             </div>
                             <div class="form-group">
-                                <label class="form-control-label" for="ckeditor">{{ __('Content') }}</label>
+                                <label class="form-control-label" for="ckeditor">{{ __('qrlanding.content') }}</label>
                                 <textarea class="form-control" id="ckeditor" name="ckeditor">{{ $page->content }}</textarea>
                             </div>
                             <div class="text-center">
-                                <button type="submit" class="btn btn-success mt-4">{{ __('Save') }}</button>
+                                <button type="submit" class="btn btn-success mt-4">{{ __('qrlanding.save') }}</button>
                             </div>
                         </form>
                     </div>
